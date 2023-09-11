6ba203030e0c16f1a0636af0b2d5c2808f0ceebb
Tommy Ivarsson <tommy.ivarsson@gmail.com>
2023-09-11 23:47:13 +0200

    Set UserTasks from seeds

--- lib/matsnet/tasks/user_tasks.ex
+++ lib/matsnet/tasks/user_tasks.ex
@@ -1,9 +1,17 @@
 defmodule Matsnet.UserTasks do
   use Ecto.Schema
+  import Ecto.Changeset
 
+  @primary_key false
   schema "user_tasks" do
-    belongs_to :user, Matsnet.Accounts.User
-    belongs_to :task, Matsnet.Tasks.Task
+    belongs_to :user, Matsnet.Accounts.User, primary_key: true
+    belongs_to :task, Matsnet.Tasks.Task, primary_key: true
     timestamps()
+  end
+
+  def changeset(user_task, attrs) do
+    user_task
+    |> cast(attrs, [:user_id, :task_id])
+    |> validate_required([:user_id, :task_id])
   end
 end
